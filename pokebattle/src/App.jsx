d1ff524b459311161be7c0ee0bf37a4dc1116947
Parker Tootill <parker.tootill@gmail.com>
2025-08-06 15:12:12 -0400

    Added HTML

--- pokebattle/src/App.jsx
+++ pokebattle/src/App.jsx
@@ -1,13 +1,40 @@
+import {PokeBattle} from "./component/PokeBattle.jsx"
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
-    </>
-  );
-}
+return (<div><PokeBattle/></div>)
+} 
+
+// function PokeBattle({ onPlayAgain }) {
+//  return (
+//     <div className="pokebattle-container">
+//         <h1 className="title"> PokeBattle</h1>
+
+//     <div className="battle-selection">
+//         {/* Top Pokemon */}
+//         <div className="pokemon-row">
+//             <div className="result-icon" />
+//             <div className="pokemon-image"> Pokemon Picture</div>
+//         </div>
+
+//         {/* VS text */}
+
+//     <div className="vs-lable">
+//         <p>Checkmark if Right</p>
+//         <p>X if Wrong</p>
+//         <h2>VS</h2>
+//     </div>
+
+//     {/* Bottom Pokemon */}
+//     <div className="pokemon-row">
+//         <div className="result-icon" />
+//         <div className="pokemon-image"> Pokemon Picture</div>
+//     </div>
+//   </div>
+
+//   <button className="play-again-buton" onClick={onPlayAgain}>Play Again</button>
+//     </div>
+//     );
+//   }
 
 export default App
